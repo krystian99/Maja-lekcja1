78ffc51ffc81a30489d1a64ec171ccf76dc2f833
skylerr091 <skylerr091@gmail.com>
2025-02-26 19:35:36 +0100

    fsgsgs

--- Maja lekcja1/Source.cpp
+++ Maja lekcja1/Source.cpp
@@ -4,34 +4,18 @@ using namespace std;
 
 // Zrob program ktory po wczytaniu liczby n wyswietli wszystkie liczby wczytane na ekran
 
-const int N = 1001; // maksymalna liczba elementów
-
 int main()
 {
-	int n;
-
-	cout << " Podaj ilosc liczb calkowitych ";
+	int n; //deklaracja liczby calkowitej
+	cout << "Podaj ilosc liczb calkowitych ";
 	cin >> n;
+	int tab[n];
 
-	int tab[N] = {};
-
-	// wczytywanie danych do tablicy
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> tab[i];
 	}
-
-	// wyswietlanie elementow tablicy
-
-	for (int i = 0; i < n; ++i)
-	{
-		cout << tab[i] << " ";
-	}
-
-	// zmieniam jeden z elementow i wyswietlam go
-	tab[0] = 123124;
-
-	cout << tab[0];
+	tab[0]=123456;
 
 	return 0;
 }
