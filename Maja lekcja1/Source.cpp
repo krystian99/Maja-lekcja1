4928fd5f1c140e6f07ca4afc9f242ce1248cb71c
skylerr091 <skylerr091@gmail.com>
2025-03-02 16:02:44 +0100

    asda

--- Maja lekcja1/Source.cpp
+++ Maja lekcja1/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib> // biblioteka do losowania liczb
 
 using namespace std;
 
@@ -9,27 +10,27 @@ const int N = 1001; // maksymalna liczba elementów
 int main()
 {
 	int n;
-	cin >> n;
 
-	int tab[N] = {};
 
-	// wczytywanie danych do tablicy
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> tab[i];
-	}
+	srand(time(0)); // generator maszyny losuj¹cej
+	int a = rand(); // funkcja rand
 
-	// wyswietlanie elementow tablicy
+	// petla for, uzycie funkcji rand(), wypisanie po spacjif
+
+	// petla for, klamry
 
 	for (int i = 0; i < n; ++i)
 	{
-		cout << tab[i] << " ";
+		int a = rand();
+		cout << a << endl;
 	}
+
+
 
 	// zmieniam jeden z elementow i wyswietlam go
-	tab[0] = 123124;
+	//tab[0] = 123124;
 
-	cout << tab[0];
+	//cout << tab[0];
 
 	return 0;
 }
