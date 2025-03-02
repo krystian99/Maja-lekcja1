783cdf8694055d14f7c82fedcab07c5f222b8b8a
skylerr091 <skylerr091@gmail.com>
2025-03-02 15:27:30 +0100

    asda

--- Maja lekcja1/Source.cpp
+++ Maja lekcja1/Source.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+const int MAX = 1001;
+int tab[MAX];
+
 // Zrob program ktory po wczytaniu liczby n wyswietli wszystkie liczby wczytane na ekran
 
 int main()
@@ -9,12 +12,23 @@ int main()
 	int n; //deklaracja liczby calkowitej
 	cout << "Podaj ilosc liczb calkowitych ";
 	cin >> n;
-	int tab[n];
+	//int tab[n];
 
+	// wczytanie do tablicy
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> tab[i];
 	}
+
+	// petla for, klamry, kolejne kroki petli
+
+	for(int i= 0; i < n; ++i)
+	{
+		cout << tab[i] << endl;
+	}
+
+
+
 	tab[0]=123456;
 
 	return 0;
