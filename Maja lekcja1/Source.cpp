724a90bc0e4385fa2942ab24d4abddb28b14515c
skylerr091 <skylerr091@gmail.com>
2025-03-02 16:34:55 +0100

    asda

--- Maja lekcja1/Source.cpp
+++ Maja lekcja1/Source.cpp
@@ -1,36 +1,20 @@
 #include <iostream>
-#include <cstdlib> // biblioteka do losowania liczb
-
+#include <cstdlib>
 using namespace std;
 
-// Zrob program ktory po wczytaniu liczby n wyswietli wszystkie liczby wczytane na ekran
 
-const int N = 1001; // maksymalna liczba elementów
 
 int main()
 {
+	
+	srand(time(NULL));
 	int n;
-
-
-	srand(time(0)); // generator maszyny losuj¹cej
-	int a = rand(); // funkcja rand
-
-	// petla for, uzycie funkcji rand(), wypisanie po spacjif
-
-	// petla for, klamry
+	cin >> n;
 
 	for (int i = 0; i < n; ++i)
 	{
 		int a = rand();
 		cout << a << endl;
 	}
-
-
-
-	// zmieniam jeden z elementow i wyswietlam go
-	//tab[0] = 123124;
-
-	//cout << tab[0];
-
 	return 0;
 }
